9836480702f0ca487bd0fbc8687117a2181903bf
Mikhail Solomin <m.solomin@vk.team>
2024-05-14 22:56:49 +0300

    test autoload

--- scripts/deployAirdrop.ts
+++ scripts/deployAirdrop.ts
@@ -1,23 +1,43 @@
-import { Address, beginCell, toNano } from '@ton/core';
+import { Address, address, beginCell, toNano } from '@ton/core';
 import { Airdrop, AirdropEntry, generateEntriesDictionary } from '../wrappers/Airdrop';
 import { compile, NetworkProvider } from '@ton/blueprint';
 import { JettonMinter } from '../wrappers/JettonMinter';
+import participants from '../airdrop.json';
 
 export async function run(provider: NetworkProvider) {
-    var entries: AirdropEntry[] = [
-        {
-            address: Address.parse('UQAN-LBI1Q8LJ8bvYk8UUm9tagO85YhJOPQk83Dq6h3-Hg7l'),
-            amount: toNano('3'),
-        },
-        {
-            address: Address.parse('UQCEp2lf1Psx4LhVliNLX_iJpoEawzM5HBAXq5gbdVsQDHFm'),
-            amount: toNano('3'),
-        },
-        {
-            address: Address.parse('UQD1KtE82GOj2a-RZqT-q1pd40rByBqMF_5gAVg7zskeJ-S_'),
-            amount: toNano('3'),
+    // console.log(participants);
+    
+    var entries: AirdropEntry[] = []
+    //     {
+    //         address: Address.parse('UQAN-LBI1Q8LJ8bvYk8UUm9tagO85YhJOPQk83Dq6h3-Hg7l'),
+    //         amount: toNano('3'),
+    //     },
+    //     {
+    //         address: Address.parse('UQCEp2lf1Psx4LhVliNLX_iJpoEawzM5HBAXq5gbdVsQDHFm'),
+    //         amount: toNano('3'),
+    //     },
+    //     {
+    //         address: Address.parse('UQD1KtE82GOj2a-RZqT-q1pd40rByBqMF_5gAVg7zskeJ-S_'),
+    //         amount: toNano('3'),
+    //     }
+    // ];
+    participants.forEach(element => {
+        try{
+            entries.push(
+                {
+                    address: Address.parse(element.address),
+                    amount: toNano(element.sum/100000)
+                }
+            )
         }
-    ];
+        catch{
+            console.log('wrong address');
+            
+        }
+        
+    });
+    console.log(entries);
+    
     // for (let index = 0; index < 15000; index++) {
     //     entries.push({
     //         address: Address.parse('UQAzfuaA6O9PqB68bm8jXRPoEywXEx5jgqTR6xT_hcPX9Gyw'),
@@ -31,7 +51,8 @@ export async function run(provider: NetworkProvider) {
 
     const dict = generateEntriesDictionary(entries);
     const dictCell = beginCell().storeDictDirect(dict).endCell();
-    console.log(`Dictionary cell (store it somewhere on your backend: ${dictCell.toBoc().toString('base64')}`);
+    // console.log(`Dictionary cell (store it somewhere on your backend: ${dictCell.toBoc().toString('base64')}`);
+    
     const merkleRoot = BigInt('0x' + dictCell.hash().toString('hex'));
 
     const jettonMinterAddress = Address.parse('EQCrAYQnZsbF2I0j7OTPVX-cILNdLFSqtmhbo82Wc6WcctEc');
@@ -46,7 +67,12 @@ export async function run(provider: NetworkProvider) {
             await compile('Airdrop')
         )
     );
-
+    var fs = require('fs');
+    fs.writeFile('data.json', JSON.stringify({address: airdrop.address.toString() ,cell: dictCell.toBoc().toString('base64')}), 'utf8', function(err: any) {
+        if (err) throw err;
+            console.log('complete');
+        });
+    
     await airdrop.sendDeploy(provider.sender(), toNano('0.05'), await jettonMinter.getWalletAddressOf(airdrop.address));
 
     await provider.waitForDeploy(airdrop.address,20,2000);
